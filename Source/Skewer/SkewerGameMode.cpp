8fe6d22eae2330b581a271e9df0dd2d3c75aa3db
Joe Zabriskie <josephzabriskie@gmail.com>
2020-07-05 18:52:34 -0600

    Fixed error on project/ game launch

--- Source/Skewer/SkewerGameMode.cpp
+++ Source/Skewer/SkewerGameMode.cpp
@@ -7,9 +7,10 @@
 ASkewerGameMode::ASkewerGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+	//	DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
+	//Hey I think I deleted that class ... whoops lol
 }
