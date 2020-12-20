012dd59878c097061e2c5ec5b9bddcb0e05d1511
Joe Zabriskie <josephzabriskie@gmail.com>
2020-12-20 14:56:32 -0700

    Ability cooldowns are saved between plays!

---
+++ Source/Skewer/GameGlobalSettings.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameGlobalSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKEWER_API UGameGlobalSettings : public UDataAsset
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere)
+};
